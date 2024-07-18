c1c8f6c88d5f9993afb48995c75e3ddfefd46d08
Alex <alex.podikoglou@gmail.com>
2024-07-19 01:10:58 +0300

    feat: add categories to flags

--- cmd/lund/main.go
+++ cmd/lund/main.go
@@ -20,50 +20,57 @@ func main() {
 		Flags: []cli.Flag{
 			// server
 			&cli.StringFlag{
-				Name:    "hostname",
-				Aliases: []string{"host"},
-				Value:   "127.0.0.1",
-				Usage:   "Host to run the server on",
-				EnvVars: []string{"HOSTNAME"},
+				Name:     "hostname",
+				Aliases:  []string{"host"},
+				Value:    "127.0.0.1",
+				Usage:    "Host to run the server on",
+				Category: "Server:",
+				EnvVars:  []string{"HOSTNAME"},
 			},
 			&cli.IntFlag{
-				Name:    "port",
-				Aliases: []string{"p"},
-				Value:   8080,
-				Usage:   "Port to run the server on",
-				EnvVars: []string{"PORT"},
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Value:    8080,
+				Usage:    "Port to run the server on",
+				Category: "Server:",
+				EnvVars:  []string{"PORT"},
 			},
 
 			// health checking
 			&cli.DurationFlag{
-				Name:    "health-check-interval",
-				Usage:   "Health Check Interval",
-				Value:   time.Second * 5,
-				EnvVars: []string{"HEALTH_CHECK_INTERVAL"},
+				Name:     "health-check-interval",
+				Usage:    "Health Check Interval",
+				Value:    time.Second * 5,
+				Category: "Health Checking:",
+				EnvVars:  []string{"HEALTH_CHECK_INTERVAL"},
 			},
 			&cli.DurationFlag{
-				Name:    "health-check-write-timeout",
-				Usage:   "Write timeout for health checking",
-				Value:   time.Millisecond * 300,
-				EnvVars: []string{"HEALTH_CHECK_WRITE_TIMEOUT"},
+				Name:     "health-check-write-timeout",
+				Usage:    "Write timeout for health checking",
+				Value:    time.Millisecond * 300,
+				Category: "Health Checking:",
+				EnvVars:  []string{"HEALTH_CHECK_WRITE_TIMEOUT"},
 			},
 			&cli.DurationFlag{
-				Name:    "health-check-read-timeout",
-				Usage:   "Read timeout for the health checking",
-				Value:   time.Millisecond * 300,
-				EnvVars: []string{"HEALTH_CHECK_READ_TIMEOUT"},
+				Name:     "health-check-read-timeout",
+				Usage:    "Read timeout for the health checking",
+				Value:    time.Millisecond * 300,
+				Category: "Health Checking:",
+				EnvVars:  []string{"HEALTH_CHECK_READ_TIMEOUT"},
 			},
 			&cli.DurationFlag{
-				Name:    "health-check-dns-cache-duration",
-				Usage:   "How often to clear the DNS cache for the Health Check component",
-				Value:   time.Millisecond * 300,
-				EnvVars: []string{"HEALTH_CHECK_DNS_CACHE_DURATION"},
+				Name:     "health-check-dns-cache-duration",
+				Usage:    "How often to clear the DNS cache for the Health Check component",
+				Value:    time.Millisecond * 300,
+				Category: "Health Checking:",
+				EnvVars:  []string{"HEALTH_CHECK_DNS_CACHE_DURATION"},
 			},
 			&cli.IntFlag{
-				Name:    "health-check-concurrency",
-				Usage:   "How many Health Checks can run at the same time",
-				Value:   4,
-				EnvVars: []string{"HEALTH_CHECK_CONCURRENCY"},
+				Name:     "health-check-concurrency",
+				Usage:    "How many Health Checks can run at the same time",
+				Value:    4,
+				Category: "Health Checking:",
+				EnvVars:  []string{"HEALTH_CHECK_CONCURRENCY"},
 			},
 
 			// discovery
@@ -71,6 +78,7 @@ func main() {
 				Name:     "discovery-mode",
 				Usage:    "Discovery Mode (possible values: docker, manual)",
 				Required: true,
+				Category: "Discovery:",
 				EnvVars:  []string{"DISCOVERY_MODE"},
 				Action: func(c *cli.Context, val string) error {
 					if val != "docker" && val != "manual" {
@@ -86,9 +94,10 @@ func main() {
 			},
 
 			&cli.StringSliceFlag{
-				Name:    "discovery-servers",
-				Usage:   "List of servers (URLs) -- use commas for separation (only applicable with manual discovery mode)",
-				EnvVars: []string{"DISCOVERY_SERVERS"},
+				Name:     "discovery-servers",
+				Usage:    "List of servers (URLs) -- use commas for separation (only applicable with manual discovery mode)",
+				Category: "Discovery:",
+				EnvVars:  []string{"DISCOVERY_SERVERS"},
 				Action: func(c *cli.Context, val []string) error {
 					// try to parse every url given, and if an error is found, return it
 					for _, v := range val {
@@ -108,10 +117,11 @@ func main() {
 			},
 
 			&cli.StringFlag{
-				Name:    "discovery-docker-sock",
-				Usage:   "Docker Socket Path",
-				Value:   "/var/run/docker.sock",
-				EnvVars: []string{"DISCOVERY_DOCKER_SOCK"},
+				Name:     "discovery-docker-sock",
+				Usage:    "Docker Socket Path",
+				Value:    "/var/run/docker.sock",
+				Category: "Discovery:",
+				EnvVars:  []string{"DISCOVERY_DOCKER_SOCK"},
 			},
 		},
 		Action: run,
